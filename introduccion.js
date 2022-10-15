31bc710881b988aac83049e62507c59bb8a0a6a8
Misato15 <salvadorchinchilla03@gmail.com>
2022-10-14 19:13:32 -0600

    Mi primer commit con git

---
+++ introduccion.js
@@ -0,0 +1,62 @@
+//-->sirve para comentar una linea
+/*
+Abre un comentario
+
+Cierra el comentario
+
+*/
+//VARIABLE
+
+/*
+Lenguajes de programacion tipados: JAVA, C++, c
+integer
+string
+boolean
+
+Lenguaje de programacion no tipado
+--Superset TypeScript
+
+
+plabras reservadas
+
+let: declara la variable en su entorno locar
+var: declara la variable en un entorno global "EVITAR UTILIZARLA"
+const: es una variable estatica o fija
+*/
+
+const numero_dia = 5;
+//numero_dia = 10; 
+console.log(numero_dia)
+
+let nombre = 'Alejandro';
+console.log(nombre)
+
+nombre = 'Luis';
+console.log(nombre);
+if(true)
+{
+ let nombre = 'Juan';
+ nombre = 'Maria';
+ this.nombre ='Gustavo';
+}
+
+console.log(nombre)
+
+if(true)
+{
+    let edad=20;
+    console.log(edad)
+}
+
+var saludo = 'Hola';
+console.log(saludo)
+
+function saludar()
+{
+this.nombre = 'Gustavo';
+console.log(this.nombre)
+let nombre = 'Linda';
+console.log(this.nombre,nombre)
+
+}
+saludar();
